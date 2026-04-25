264b9b2decf93c2cc8825c440d3665966b147de1
Ayomide <ayomidemamukuyomi5@gmail.com>
2026-04-25 11:29:20 +0100

    Did the Reports pages

--- frontend/src/components/ReportFound.jsx
+++ frontend/src/components/ReportFound.jsx
@@ -5,6 +5,22 @@ const ReportFound = () => {
   return (
     <>
     <NavBar />
+      <section >
+        <div className='report-form'>
+        <h1>Found Item Details</h1>
+        <p>Item Name</p>
+        <input type="text" placeholder='e.g. Black Wallet '/>
+        <p>Upload Found Item Image (Upload if Available)</p>
+        <input type="file" accept="image/*"/>
+        <p>Description (colour, brand, size, markings)</p>
+        <textarea type="text" placeholder='Describe the item in detail..'/>
+        <p>Date Found</p>
+        <input type="date"/>
+        <p>Location Found</p>
+        <input type="text" placeholder='Item Description'/>
+        <button>Submit Found Item Report</button>
+      </div>
+      </section>
     </>
   )
 }
--- frontend/src/components/ReportLost.jsx
+++ frontend/src/components/ReportLost.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import NavBar from './NavBar'
+import "../css/ReportLost.css"
 
 const ReportLost = () => {
   return (
     <>
       <NavBar/>
-      
+      <section >
+        <div className='report-form'>
+        <h1>Lost Item Details</h1>
+        <p>Item Name</p>
+        <input type="text" placeholder='e.g. Black Wallet '/>
+        <p>Upload Lost Item Image (Upload if Available)</p>
+        <input type="file" accept="image/*"/>
+        <p>Description (colour, brand, size, markings)</p>
+        <textarea type="text" placeholder='Describe the item in detail..'/>
+        <p>Date Lost</p>
+        <input type="date"/>
+        <p>Last Known Location</p>
+        <input type="text" placeholder='Item Description'/>
+        <button>Submit Lost Item Report</button>
+      </div>
+      </section>
     </>
   )
 }
